1c81c43bc6ac06792099c33fc3e71ef90193dbb9
James Corfe <james.corfe@gmail.com>
2023-04-12 15:07:02 +0100

    Ingredients view page and search bar

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,24 @@
 class IngredientsController < ApplicationController
+
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def search
+    if params[:query].present?
+      @ingredients = Ingredient.where("ingredient LIKE ?", "%#{params[:query]}%")
+    else
+      @ingredients = Ingredient.all
+    end
+
+    render turbo_stream: turbo_stream.replace(
+      'ingredients',
+      partial: 'list',
+      locals: {
+        ingredients: @ingredients
+      }
+    )
+  end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :cocktails
+  resources :ingredients do
+    collection do
+      post 'search'
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
